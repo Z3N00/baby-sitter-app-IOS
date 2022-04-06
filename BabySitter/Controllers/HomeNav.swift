f71d7aaec854e66e4dd3385d7550e6015b9f19db
Sahil <sahilgirdhar410@gmail.com>
2022-04-06 10:10:38 -0400

    added home nav screen

---
+++ BabySitter/Controllers/HomeNav.swift
@@ -0,0 +1,29 @@
+//
+//  HomeNav.swift
+//  BabySitter
+//
+//  Created by 2022M3 on 27/03/22.
+//
+
+import UIKit
+
+class HomeNav: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
